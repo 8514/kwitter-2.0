6404457a212dc17364bce3eb82b286331002599f
8514 <79288367+8514@users.noreply.github.com>
2021-07-08 22:26:52 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -16,7 +16,13 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
+firebase.analytics();
+function redirectToRoomName(name){
+localStorage.setItem("room_name",name);
+window.location="kwitter_page.html";
+}
